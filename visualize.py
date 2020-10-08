618997afa4210ac371be3ef1ba6c54fb36666c5a
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-10-07 22:12:40 -0400

    Added Dumping to CSV

--- visualize.py
+++ visualize.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 date = []
 file = []
+change_type = []
 lines_added = []
 lines_removed = []
 repo = RepositoryMining(path_to_repo="https://github.com/TheRenegadeCoder/sample-programs.git")
@@ -10,8 +11,18 @@ for commit in repo.traverse_commits():
     for modification in commit.modifications:
         date.append(commit.committer_date)
         file.append(modification.filename)
+        change_type.append(modification.change_type)
         lines_added.append(modification.added)
         lines_removed.append(modification.removed)
 
-df = pd.DataFrame(data={"date": date, "file": file})
-print(df)
+df = pd.DataFrame(
+    data={
+        "date": date,
+        "file": file,
+        "change_type": change_type,
+        "added": lines_added,
+        "removed": lines_removed
+    }
+)
+
+df.to_csv("git_data.csv")
