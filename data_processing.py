12131e041adb4e5a571367eb37b530b74431f204
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-10-07 23:29:07 -0400

    Added basic language detection

--- data_processing.py
+++ data_processing.py
@@ -1,19 +1,27 @@
 from pydriller import RepositoryMining
 import pandas as pd
+import os
+
+languages = {
+    ".py": "Python"
+}
 
 date = []
 file = []
 change_type = []
 lines_added = []
 lines_removed = []
+language = []
 repo = RepositoryMining(path_to_repo="https://github.com/TheRenegadeCoder/sample-programs.git")
 for commit in repo.traverse_commits():
     for modification in commit.modifications:
+        _, ext = os.path.splitext(modification.filename)
         date.append(commit.committer_date)
         file.append(modification.filename)
         change_type.append(modification.change_type.name)
         lines_added.append(modification.added)
         lines_removed.append(modification.removed)
+        language.append(languages.get(ext))
 
 df = pd.DataFrame(
     data={
@@ -21,7 +29,8 @@ df = pd.DataFrame(
         "file": file,
         "change_type": change_type,
         "added": lines_added,
-        "removed": lines_removed
+        "removed": lines_removed,
+        "language": language
     }
 )
 
