28730f8a6e858cd817341879a0904403f6716a6c
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-10-07 21:54:39 -0400

    Generated a DataFrame

--- visualize.py
+++ visualize.py
@@ -1,4 +1,17 @@
 from pydriller import RepositoryMining
+import pandas as pd
+
+date = []
+file = []
+lines_added = []
+lines_removed = []
 repo = RepositoryMining(path_to_repo="https://github.com/TheRenegadeCoder/sample-programs.git")
 for commit in repo.traverse_commits():
-    print('Hash {}, author {}'.format(commit.hash, commit.author.name))
+    for modification in commit.modifications:
+        date.append(commit.committer_date)
+        file.append(modification.filename)
+        lines_added.append(modification.added)
+        lines_removed.append(modification.removed)
+
+df = pd.DataFrame(data={"date": date, "file": file})
+print(df)
